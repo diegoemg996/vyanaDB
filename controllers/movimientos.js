634f14f87951b1a7db48a83bc7ded2df774679dc
Diego Mendoza <diegoemg996@gmail.com>
2021-05-04 08:58:13 -0500

    movimientos controller modificado

--- controllers/movimientos.js
+++ controllers/movimientos.js
@@ -28,31 +28,6 @@ const entradaProducto = async(req, res = response) =>{
     })
 }
 
-const salidaProducto = async(req, res = response) =>{
-
-    const {cantidad} = req.body;
-
-    const {id} = req.params;
-
-    await Producto.findOneAndUpdate({_id :id}, {$inc : {'cantidad' : cantidad}});
-
-    const data = {
-        nombre: id,
-        cantidad,
-        tipo: "entrada",
-        usuario: req.usuario._id
-
-    }
-
-    const movimientos = new Movimiento( data );
-
-    await movimientos.save();
-
-    res.json({
-        movimientos
-    })
-}
-
 module.exports = {
     entradaProducto
 }
