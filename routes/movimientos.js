b62e006c008609d3f0afeef5e4ad669c066dcbe4
Diego Mendoza <diegoemg996@gmail.com>
2021-05-07 15:29:43 -0500

    modificacion validaciones

--- routes/movimientos.js
+++ routes/movimientos.js
@@ -3,16 +3,13 @@ const { check } = require('express-validator');
 const {entradaProducto} = require('../controllers/movimientos')
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
+/* const { validarJWT } = require('../middlewares/validar-jwt'); */
 
 const router = Router();
 
 
 router.post('/agregar/:id',[
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('cantidad', 'La cantidad es obligatoria').not().isEmpty(),
-    check('usuario', 'El usuario es obligatorio es obligatoria').not().isEmpty(),
-    check('tipo', 'El tipo es obligatorio').not().isEmpty(),
     validarCampos
 ],entradaProducto);
 
